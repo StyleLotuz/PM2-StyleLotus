fe82e9edb31cf00eae6493e5e3ffd1d27d6f9d66
StyleLotus <106494680+StyleLotus@users.noreply.github.com>
2024-04-11 04:55:24 -0500

    HW4 Done
    We should think about the genre for the movie card

---
+++ front/scripts/renderCards.js
@@ -0,0 +1,45 @@
+const renderMovies = (movie) => {
+    const container = document.getElementById("cardContainer");
+  
+    const card = document.createElement("div");
+    card.id = "card";
+  
+    const img = document.createElement("img");
+    img.src = movie.poster;
+    img.alt = movie.title;
+  
+    const titleP = document.createElement("p");
+    titleP.innerText = movie.title + " ";
+  
+    const year = document.createElement("span");
+    year.innerText = movie.year;
+  
+    titleP.appendChild(year);
+  
+    const durationP = document.createElement("p");
+    durationP.innerText = "DURATION: ";
+    const durationSpan = document.createElement("span");
+    durationSpan.innerText = movie.duration;
+    durationP.appendChild(durationSpan);
+  
+    const directorP = document.createElement("p");
+    directorP.innerText = "DIRECTOR: ";
+    const directorSpan = document.createElement("span");
+    directorSpan.innerText = movie.director;
+    directorP.appendChild(directorSpan);
+  
+    const rateSpan = document.createElement("span");
+    const rateP = document.createElement("p");
+    rateP.innerText = movie.rate;
+    rateSpan.appendChild(rateP);
+  
+    card.appendChild(img);
+    card.appendChild(titleP);
+    card.appendChild(durationP);
+    card.appendChild(directorP);
+    card.appendChild(rateSpan);
+  
+    container.appendChild(card);
+  };
+
+  module.exports = renderMovies
---
+++ front/scripts/slider.js
@@ -0,0 +1,19 @@
+const sliderAnimation = () => {
+  const sliderInner = document.querySelector(".slider--inner");
+  console.log(sliderInner);
+  let sliderIndex = 0;
+
+  let images = sliderInner.querySelectorAll("img");
+
+  setInterval(() => {
+    let percentage = sliderIndex * -100;
+    sliderInner.style.transform = "translateX(" + percentage + "%)";
+    sliderIndex++;
+    if (sliderIndex > images.length - 1) {
+      sliderIndex = 0;
+    }
+  }, 5000);
+};
+
+
+module.exports = sliderAnimation
