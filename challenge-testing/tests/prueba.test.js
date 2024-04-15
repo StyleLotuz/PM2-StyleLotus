5c86b49b50934fd96ada7f3f6f3465d00043bf38
StyleLotus <106494680+StyleLotus@users.noreply.github.com>
2024-04-15 18:14:03 -0500

    Some Change on testing

--- challenge-testing/tests/prueba.test.js
+++ challenge-testing/tests/prueba.test.js
@@ -2,48 +2,45 @@ const CarritoCompra = require("../index");
 
 describe("Probando la funcion CarritoCompra", () => {
   let carrito;
+  const producto1 = { nombre: "Camiseta", valor: 20, cantidad: 3 };
+  const producto2 = { nombre: "Pantalón", valor: 35, cantidad: 2 };
+  const producto3 = { nombre: "Zapatillas", valor: 50, cantidad: 1 };
 
   beforeEach(() => {
     carrito = new CarritoCompra();
   });
+
+  it("Debe ser una clase nuestro carrito compra", ()=>{
+    expect(typeof CarritoCompra.prototype.constructor).toBe("function")
+  })
 
   it("Inicializa el carrito como un array vacio", () => {
     expect(carrito.carrito).toEqual([]);
   });
 
   it("Recibe un objeto representando un producto y lo agrega al carrito.", () => {
-    const producto1 = {nombre: "Camiseta",valor: 20,cantidad: 3};
-    const producto2 = {nombre: "Pantalón", valor: 35, cantidad: 2};
-    const producto3 = {nombre: "Zapatillas", valor: 50, cantidad: 1};
-    carrito.agregarProducto(producto1)
-    carrito.agregarProducto(producto2)
-    carrito.agregarProducto(producto3)
+    carrito.agregarProducto(producto1);
+    carrito.agregarProducto(producto2);
+    carrito.agregarProducto(producto3);
 
-    expect(carrito.carrito).toContain(producto1)
-    expect(carrito.carrito).toContain(producto2)
-    expect(carrito.carrito).toContain(producto3)
+    expect(carrito.carrito).toContain(producto1);
+    expect(carrito.carrito).toContain(producto2);
+    expect(carrito.carrito).toContain(producto3);
   });
 
-  it("Calcula el total de la compra sumando los precios de todos los productos en el carrito.", ()=>{
-    const producto1 = {nombre: "Camiseta",valor: 20,cantidad: 3};
-    const producto2 = {nombre: "Pantalón", valor: 35, cantidad: 2};
-    const producto3 = {nombre: "Zapatillas", valor: 50, cantidad: 1};
-    carrito.agregarProducto(producto1)
-    carrito.agregarProducto(producto2)
-    carrito.agregarProducto(producto3)
+  it("Calcula el total de la compra sumando los precios de todos los productos en el carrito.", () => {
+    carrito.agregarProducto(producto1);
+    carrito.agregarProducto(producto2);
+    carrito.agregarProducto(producto3);
 
-    expect(carrito.calcularTotal()).toBe(180)
-  })
+    expect(carrito.calcularTotal()).toBe(180);
+  });
 
-  it("Aplica un descuento al total de la compra según el porcentaje especificado.", ()=>{
-    const producto1 = {nombre: "Camiseta",valor: 20,cantidad: 3};
-    const producto2 = {nombre: "Pantalón", valor: 35, cantidad: 2};
-    const producto3 = {nombre: "Zapatillas", valor: 50, cantidad: 1};
-    
-    carrito.agregarProducto(producto1)
-    carrito.agregarProducto(producto2)
-    carrito.agregarProducto(producto3)
+  it("Aplica un descuento al total de la compra según el porcentaje especificado.", () => {
+    carrito.agregarProducto(producto1);
+    carrito.agregarProducto(producto2);
+    carrito.agregarProducto(producto3);
 
-    expect(carrito.aplicarDescuento(20)).toBe(144)
-  })
+    expect(carrito.aplicarDescuento(20)).toBe(144);
+  });
 });
