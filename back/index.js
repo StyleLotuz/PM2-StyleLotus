8df837f868f9bc28b147eb26589e9cb596f4d046
StyleLotus <106494680+StyleLotus@users.noreply.github.com>
2024-04-18 12:05:39 -0500

    HW10 Done

--- back/index.js
+++ back/index.js
@@ -1,5 +1,10 @@
+const dbCon = require("./config/dbCon");
 const app = require("./src/server");
 
-app.listen(3000, () => {
-  console.log("El servidor se esta escuchando en el puerto 3000");
-});
+dbCon().then((res)=>{
+  app.listen(3000, () => {
+    console.log("El servidor se esta escuchando en el puerto 3000");
+  });
+}).catch((error)=>{
+  console.log("Hubo un error al conectar con la base datos", error)
+})
--- back/src/controllers/moviesController.js
+++ back/src/controllers/moviesController.js
@@ -1,8 +1,12 @@
-const moviesServices = require("../services/moviesServices")
+const moviesServices = require("../services/moviesServices");
 
 module.exports = {
-    getAllMovies : async (req, res) =>{
-        const movies = await moviesServices.getMovies();
-        res.status(200).json(movies)
+  getAllMovies: async (req, res) => {
+    try {
+      const movies = await moviesServices.getMovies();
+      res.status(200).json(movies);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
     }
-}
+  },
+};
